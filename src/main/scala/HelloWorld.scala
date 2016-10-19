853357e7c38da94dd5e53efb4a3e4130f4dfe0ab
Himanshu ARORA <himanshu.arora.alw@gmail.com>
2016-10-19 11:50:59 +0200

    Update HelloWorld.scala

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 /**
  * Object to create hello world rest ful api.
+ * Run this object and goto http://localhost:8080/ to test this API.
  */
 object HelloWorld {
   def main(args: Array[String]) {
